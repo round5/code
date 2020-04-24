3fc6bfa9cde82f025b18e561bddce4238c181210
SergeySeroshtan <sergey.seroshtan@gmail.com>
2020-04-24 20:03:07 +0300

    Fix naked AVX2 instructions

--- reference/src/common/fips202/1x/keccakf1600.h
+++ reference/src/common/fips202/1x/keccakf1600.h
@@ -3,9 +3,12 @@
 
 //	Importing, build upon:
 
-#include <stddef.h> 
+#include <stddef.h>
 #include <stdint.h>
+
+#ifdef AVX2
 #include <immintrin.h>
+#endif
 
 
 #include "chooseparameters.h"
@@ -17,6 +20,8 @@ void KeccakF1600_StateExtractBytes ( uint64_t *state, uint8_t *data Parameters )
 void KeccakF1600_StateXORBytes ( uint64_t *state, const uint8_t *data Parameters );
 
 void KeccakF1600_StatePermute( uint64_t *state );
+
+#ifdef AVX2
 
 void KeccakF1600_StateExtractBytes_4x(__m256i *state,
                                       uint8_t *data0,
@@ -31,6 +36,8 @@ void KeccakF1600_StateXORBytes_4x (__m256i *state,
                                    const uint8_t *data2,
                                    const uint8_t *data3
                                    Parameters );
+
+#endif /* AVX2 */
 
 #endif /* _KECCAKF1600_H_ */
 
